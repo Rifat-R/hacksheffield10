4e54ac9352001835b0c71e5a975fa8bcc0504085
Rifat Rahman <rifatrahman2003@gmail.com>
2025-11-30 09:11:20 +0000

    Adding swipes to routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from .core import core_bp
 from .products import products_bp
 from .dashboard import dashboard_bp
+from .swipes import swiped_bp
 
-__all__ = ["core_bp", "products_bp", "dashboard_bp"]
+__all__ = ["core_bp", "products_bp", "dashboard_bp", "swiped_bp"]
